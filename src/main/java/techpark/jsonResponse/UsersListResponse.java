a8b9f8d9772007a753b4f637a567b69e2e05d7a9
yaches <@>
2017-03-12 22:52:45 +0300

    remove comments

--- src/main/java/techpark/jsonResponse/UsersListResponse.java
+++ src/main/java/techpark/jsonResponse/UsersListResponse.java
@@ -18,25 +18,4 @@ public class UsersListResponse extends Response {
         super("ok");
         this.users = users;
     }
-
-//    @Override
-//    public ResponseEntity<?> getMessage() {
-//        String body = "{\"users\": [";
-//
-//        LinkedList<UserProfile> users = this.users;
-////        for (UserProfile user : users) {
-//        for (int i = 0; i < users.size(); ++i) {
-//            UserProfile user = users.get(i);
-//            body += "{";
-//            body += "\"login\": \"" + user.getLogin() + "\",";
-//            body += "\"score\": \"" + user.getScore() + "\"";
-//            body += "}";
-//            if (i != users.size() - 1) {
-//                body += ",";
-//            }
-//        }
-//
-//        body += "] }";
-//        return new ResponseEntity<>(body, HttpStatus.OK);
-//    }
 }
