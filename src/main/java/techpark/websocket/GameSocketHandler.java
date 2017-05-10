ae8461a99704b63c1e4eaa6bdc2c1d13bd699218
FVarya <F.Varya@ya.ru>
2017-05-11 00:03:43 +0300

    l -> L

--- src/main/java/techpark/websocket/GameSocketHandler.java
+++ src/main/java/techpark/websocket/GameSocketHandler.java
@@ -54,7 +54,7 @@ public class GameSocketHandler extends TextWebSocketHandler{
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws AuthenticationException
             , AccountServiceDBException {
-        final String login = (String) session.getAttributes().get("login");
+        final String login = (String) session.getAttributes().get("Login");
         final UserProfile user;
         if (login == null || (user = accountService.getUserByLogin(login)) == null) {
             throw new AuthenticationException("Only authenticated users allowed to play a game");
