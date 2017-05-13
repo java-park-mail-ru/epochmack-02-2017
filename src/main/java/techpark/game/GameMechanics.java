a4d14d0674e927f9793080c6780ff18985e782b1
yaches <slavik-kruglov@mail.ru>
2017-05-13 19:38:26 +0300

    test

--- src/main/java/techpark/game/GameMechanics.java
+++ src/main/java/techpark/game/GameMechanics.java
@@ -54,9 +54,10 @@ public class GameMechanics {
     public void addPlayer(UserProfile user){
         executor.submit(()->{
 //             if(!waiters.contains(user) && !sessions.containsKey(user)){
+            if(!waiters.contains(user)){
                 waiters.add(user);
                 searchGame();
-//             }
+            }
         });
     }
 
